d0caf0b8db96b98944318dfe527cf408a43739d5
Tim Bartos <tbartos14@gmail.com>
2019-11-12 12:28:12 -0500

    Fixes

--- SCR/data_collection.py
+++ SCR/data_collection.py
@@ -3,8 +3,9 @@
 
 import datetime
 import subprocess
+import time
 
-create_new: bool = False
+create_new: bool = True
 
 # create a identifier
 now_strf: str = datetime.datetime.now().strftime("%d-%m-%Y.%H:%M")
@@ -16,10 +17,11 @@ else:
     output_file = open(f"Trial_overwrite.csv", "w+")  # type: ignore
 
 # write header
+output_file.writelines([str(time.time())])
 output_file.writelines(["In,Out,In-Out,DeltaT"])
 
 subprocess.call(["./tof"], shell=True, stdout=output_file)
 output_file.close()
+output_file.writelines([str(time.time())])
 
-# sleep 2 && echo "TEST" & <-- final ampersand is multiprocessing
 
